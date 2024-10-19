48862fb9f5961d8fc2e7100d2a4b92b18040ee0e
Jonna Rinne <jonna.rinne1@gmail.com>
2024-10-19 15:31:32 +0300

    first commit

---
+++ src/main/java/syksy24/kulutusseuranta/KulutusseurantaApplication.java
@@ -0,0 +1,13 @@
+package syksy24.kulutusseuranta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KulutusseurantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KulutusseurantaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/syksy24/kulutusseuranta/KulutusseurantaApplicationTests.java
@@ -0,0 +1,13 @@
+package syksy24.kulutusseuranta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KulutusseurantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
